6ecbb12d68dec69d61b9132ba017747ef8eb335b
verberkj <verberkj4289@student.faytechcc.edu>
2026-08-28 20:23:46 -0400

    Add files via upload

---
+++ P1LAB1_VerBerkmoesJenni.py
@@ -0,0 +1,10 @@
+#Jenni VerBerkmoes
+#8/28/26
+#P1LAB1
+#Asks user for input and provides a brief message to welcome the user.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name,last_name + "!", "Welcome to CTI-110!")
